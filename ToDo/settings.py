a8b568178fb94a369c8a0d1691be9ac0fd02fe70
Ali Mehdizade <amehdizade111@gmail.com>
2026-06-25 00:30:46 +0400

    Prepare project for deployment

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*tuk^a&whw)td4j5-z^qj%&%efzqpu2a%ji2yj+95=heyyj&*d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,6 +120,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

e0911dd1a3662272ff33a2740ac01698decb130a
Ali Mehdizade <amehdizade111@gmail.com>
2026-09-19 23:38:55 +0400

    Fix DATABASE_URL typo in settings.py

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'ToDo.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URl"),
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True,
     )
